9728510c35aadcad388aa0f08fdadbaf95790b3d
Sinem Demirey <sinem.demirey@gmail.com>
2020-02-15 15:58:52 +0300

    Practice with CALayer

--- calayer/ViewController.swift
+++ calayer/ViewController.swift
@@ -2,22 +2,71 @@
 //  ViewController.swift
 //  calayer
 //
-//  Created by Gwinyai on 31/5/2018.
-//  Copyright © 2018 Gwinyai. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var practiceView: UIView!
+    
+    //standalone layer
+    lazy var newLayer : CALayer = {
+        return CALayer()
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupStandaloneLayer()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //Called to notify the view controller that its view has just laid out its subviews
+    //we are resizing it with the view
+    override func viewDidLayoutSubviews() {
+        //since new layer is the subview of practice view praactice view's bound will be newlayer's frame
+        newLayer.frame = practiceView.bounds
+    }
+    
+    //this layer will not automatically resize with the view
+    func setupStandaloneLayer(){
+        newLayer.backgroundColor = UIColor.purple.cgColor
+        newLayer.cornerRadius = 10.0
+        newLayer.borderWidth = 5.0
+        newLayer.borderColor = UIColor.systemPink.cgColor
+        newLayer.shadowOpacity = 0.8
+        newLayer.shadowRadius = 15.0
+        // think like light coming from right side of the screen
+        newLayer.shadowOffset = CGSize(width: CGFloat(-15), height: CGFloat(-15))
+        // even on every side
+        newLayer.shadowOffset = CGSize.zero
+        
+        newLayer.contents = UIImage(named: "cat")?.cgImage
+        // masks everything outside the bounds
+        newLayer.masksToBounds = true
+        
+        newLayer.contents = UIImage(named: "cat2")?.cgImage
+        newLayer.contentsGravity = CALayerContentsGravity.center
+        
+        practiceView.layer.addSublayer(newLayer)
+    }
+    
+    func setupNormalLayer(){
+        practiceView.layer.backgroundColor = UIColor.purple.cgColor
+        practiceView.layer.cornerRadius = 10.0
+        practiceView.layer.borderWidth = 5.0
+        practiceView.layer.borderColor = UIColor.systemPink.cgColor
+        practiceView.layer.shadowOpacity = 0.8
+        practiceView.layer.shadowRadius = 15.0
+        // think like light coming from right side of the screen
+        practiceView.layer.shadowOffset = CGSize(width: CGFloat(-15), height: CGFloat(-15))
+        // even on every side
+        practiceView.layer.shadowOffset = CGSize.zero
+        
+        practiceView.layer.contents = UIImage(named: "cat")?.cgImage
+        // masks everything outside the bounds
+        practiceView.layer.masksToBounds = true
+        
+        practiceView.layer.contents = UIImage(named: "cat2")?.cgImage
+        practiceView.layer.contentsGravity = CALayerContentsGravity.center
     }
 
 
